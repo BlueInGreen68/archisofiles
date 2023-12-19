5375c5d9e006d0cc3b733d035a7eff70788a0730
blueingreen68 <blueingreen68@gmail.com>
2023-12-19 20:17:12 +0500

    update

---
+++ install.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+set -e
+
+cloneFiles () {
+  git clone https://github.com/blueingreen68/archisofiles
+  dirArchIsoFiles=~/archisofiles
+}
+
+cloneDotfiles () {
+  keepassxc-cli show ../Yandex.Disk/keepassxc/Passwords.kdbx github | grep "Notes:" | awk '{ print $2 }' | wl-copy
+  echo "Пароль скопирован и находится в буфере обмена"
+  git clone https://github.com/blueingreen68/.dotfiles
+  dotfiles=~/.dotfiles
+}
+
+readArrays () {
+   # Удалить переменную dotfiles по завершению написания скрипта
+   dotfiles=~/.dotfiles
+   readarray -t stowPackages < <(ls -l  "$dotfiles" | grep '^d' | awk '{ print $9 }') 
+}
+
+stowPackageExtract () {
+  for package in ${stowPackages[@]}
+    do
+      packageFirstSymbol=${package:0:1}
+
+      if [ "$packageFirstSymbol" = "_" ]; then
+        stow --no-folding -vt ~ "$package"
+      else
+        stow -nvt ~ "$package" 
+      fi
+  done
+}
+
+startSetup () {
+  select event in Pacman Stow; do
+      case $event in
+		    Pacman)
+		      pacman -S --needed - < "$archisofiles"/pkglist.txt
+
+			    break
+			    ;;
+
+        Stow)
+          readArrays
+          stowPackageExtract
+
+			    break
+			    ;;
+
+        *)
+			    echo "Invalid option... Выход"
+			    exit
+			    ;;
+      esac
+  done
+}
+
+# Установка
+startSetup
+
