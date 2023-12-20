1b379ff690f173dce03f2adcf04ccac3bcca081a
blueingreen68 <blueingreen68@gmail.com>
2023-12-20 18:27:21 +0500

    update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
-
-set -e
-
 dirArchIsoFiles=~/archisofiles
 dotfiles=~/.dotfiles
+counterAbortedPkg=0
+
+setStatusE () {
+  if [ "$1" = true ]; then
+    set +e 
+  else
+    set -e
+  fi
+}
+
+setStatusE false
+
+pacmanSetupPkg () {
+	sudo pacman -S --needed - < "$archisofiles"/pkglist.txt
+}
 
 cloneDotfiles () {
   keepassxc-cli show "$dirArchIsoFiles"/Passwords.kdbx github | grep "Notes:" | awk '{ print $2 }' | wl-copy
@@ -12,38 +24,95 @@ cloneDotfiles () {
 }
 
 readArrays () {
-   readarray -t stowPackages < <(ls -l  "$dotfiles" | grep '^d' | awk '{ print $9 }') 
+   readarray -t stowPkgs < <(ls -l  "$dotfiles" | grep '^d' | awk '{ print $9 }') 
 }
 
-stowPackageExtract () {
-  for package in ${stowPackages[@]}
+checkPkg () {
+  counterAbortedPkg=$((counterAbortedPkg+1))
+
+  echo "Название: $package" >> "$dotfiles"/abortedPkg.txt 
+
+  stow -d "$dotfiles" -nvt ~ "$package" 2>&1 | awk  '{ print $11 }' >> "$dotfiles"/abortedPkg.txt 
+}
+
+stowNoFolding () {
+  setStatusE true
+
+  stow -d "$dotfiles" --no-folding -nvt ~ "$package"
+
+  if [ $? -eq 1 ]; then
+    setStatusE false
+
+    checkPkg
+  else
+    setStatusE false
+
+    stow -d "$dotfiles" --no-folding --adopt -vt ~ "$package"
+  fi
+}
+
+stowDir () {
+  setStatusE true
+
+  stow -d "$dotfiles" --adopt -nvt ~ "$package" 
+
+  if [ $? -eq 1 ]; then
+    setStatusE false
+
+    checkPkg
+  else
+    setStatusE false
+    
+    stow -d "$dotfiles" --adopt -vt ~ "$package"
+  fi
+}
+
+stowPkgExtract () {
+  for package in ${stowPkgs[@]}
     do
       packageFirstSymbol=${package:0:1}
 
       if [ "$packageFirstSymbol" = "_" ]; then
-        stow --no-folding -vt ~ "$package"
+        stowNoFolding  
       else
-        stow -nvt ~ "$package" 
+        stowDir
+      fi
+  done
+}
+
+stowUpdateNoFoldingPkg () {
+  for package in ${stowPkgs[@]}
+    do
+      packageFirstSymbol=${package:0:1}
+
+      if [ "$packageFirstSymbol" = "_" ]; then
+        stowNoFolding
       fi
   done
 }
 
 startSetup () {
-  select event in Pacman Stow; do
+  select event in Pacman Stow StowUpdate; do
       case $event in
 		    Pacman)
-		      pacman -S --needed - < "$archisofiles"/pkglist.txt
-
-			    break
+          pacmanSetupPkg
+          break
 			    ;;
 
         Stow)
+          pacmanSetupPkg
           cloneDotfiles
           readArrays
-          stowPackageExtract
+          stowPkgExtract
 
 			    break
 			    ;;
+
+        StowUpdate)
+          stowUpdateNoFoldingPkg
+
+          break 
+          ;;
 
         *)
 			    echo "Invalid option... Выход"
@@ -51,8 +120,12 @@ startSetup () {
 			    ;;
       esac
   done
+  
+  if [ "$counterAbortedPkg" -gt 0 ]; then
+    echo "Количество нераспакованных пакетов: $counterAbortedPkg"
+    echo "Список находится в ~/.dotfiles/abortedPkg.txt"
+  fi
 }
 
 # Установка
 startSetup
-
