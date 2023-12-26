eb3d8950895cb9ecaaa16cf700e5e1f94485bd67
blueingreen68 <blueingreen68@gmail.com>
2023-12-26 19:03:41 +0500

    update

--- install.sh
+++ install.sh
@@ -18,81 +18,6 @@ setStatusE () {
 
 setStatusE false
 
-readArrays () {
-   readarray -t stowPkgs < <(ls -l "$dotfiles" | grep '^d' | awk '{ print $9 }') 
-}
-
-checkPkg () {
-
-  counterAbortedPkg=$((counterAbortedPkg+1))
-
-  if [ "$counterAbortedPkg" -eq 1 ]; then echo -e "$(date +%d-%m-%Y::%T) \n" >> "$dotfiles"/abortedPkg.txt
-  fi
-  
-  echo "Название пакета: $package" >> "$dotfiles"/abortedPkg.txt 
-  stow -d "$dotfiles" -nvt ~ "$package" 2>&1 | awk  '{ print $11 }' | sed '/^[[:space:]]*$/d' >> "$dotfiles"/abortedPkg.txt 
-
-}
-
-stowNoFolding () {
-  
-  setStatusE true
-
-  stow -d "$dotfiles" --no-folding -nvt ~ "$package"
-
-  if [ $? -eq 1 ]; then
-    setStatusE false
-    checkPkg
-  else
-    setStatusE false
-    stow -d "$dotfiles" --no-folding -vt ~ "$package"
-  fi
-
-}
-
-stowDir () {
-
-  setStatusE true
-
-  stow -d "$dotfiles" -nvt ~ "$package" 
-
-  if [ $? -eq 1 ]; then
-    setStatusE false checkPkg
-  else
-    setStatusE false
-    stow -d "$dotfiles" -vt ~ "$package"
-  fi
-
-}
-
-stowPkgExtract () {
-
-  for package in ${stowPkgs[@]}
-    do
-      packageFirstSymbol=${package:0:1}
-
-      if [ "$packageFirstSymbol" = "_" ]; then
-        stowNoFolding  
-      else
-        stowDir
-      fi
-  done
-
-}
-
-stowUpdateNoFoldingPkg () {
-
-  for package in ${stowPkgs[@]}
-    do
-      packageFirstSymbol=${package:0:1}
-
-      if [ "$packageFirstSymbol" = "_" ]; then
-        stowNoFolding
-      fi
-  done
-  
-}
-
 abortedPkg () {
 
   if [ "$counterAbortedPkg" -gt 0 ]; then
@@ -295,9 +220,9 @@ rewriteAbortedPackage () {
 startSetup () {
   abortedPkg "delete"
 
-  select event in Yay Stow StowUpdate AddPackage CreateDefaultDirs; do
+  select event in "Yay setup packages" "Stow" StowUpdate AddPackage CreateDefaultDirs; do
       case $event in
-		    Yay)
+		    "Yay setup packages")
           source "$dirArchIsoFiles"/library/yaySetupPkg.sh
 
           break
@@ -305,14 +230,12 @@ startSetup () {
 
         Stow)
           source "$dirArchIsoFiles"/library/cloneDotfiles.sh
-          readArrays
           stowPkgExtract
 
 			    break
 			    ;;
 
         StowUpdate)
-          readArrays
           stowUpdateNoFoldingPkg
           
           break 
---
+++ library/stow.sh
@@ -0,0 +1,79 @@
+#!/bin/bash 
+
+readArrays () {
+   readarray -t stowPkgs < <(ls -l "$dotfiles" | grep '^d' | awk '{ print $9 }') 
+}
+
+checkPkg () {
+
+  counterAbortedPkg=$((counterAbortedPkg+1))
+
+  if [ "$counterAbortedPkg" -eq 1 ]; then echo -e "$(date +%d-%m-%Y::%T) \n" >> "$dotfiles"/abortedPkg.txt
+  fi
+  
+  echo "Название пакета: $package" >> "$dotfiles"/abortedPkg.txt 
+  stow -d "$dotfiles" -nvt ~ "$package" 2>&1 | awk  '{ print $11 }' | sed '/^[[:space:]]*$/d' >> "$dotfiles"/abortedPkg.txt 
+
+}
+
+stowNoFolding () {
+  setStatusE true
+
+  stow -d "$dotfiles" --no-folding -nvt ~ "$package"
+
+  if [ $? -eq 1 ]; then
+    setStatusE false
+    checkPkg
+  else
+    setStatusE false
+    stow -d "$dotfiles" --no-folding -vt ~ "$package"
+  fi
+
+}
+
+stowDir () {
+
+  setStatusE true
+
+  stow -d "$dotfiles" -nvt ~ "$package" 
+
+  if [ $? -eq 1 ]; then
+    setStatusE false
+    checkPkg
+  else
+    setStatusE false
+    stow -d "$dotfiles" -vt ~ "$package"
+  fi
+
+}
+
+stowPkgExtract () {
+
+  for package in ${stowPkgs[@]}
+    do
+      packageFirstSymbol=${package:0:1}
+
+      if [ "$packageFirstSymbol" = "_" ]; then
+        stowNoFolding  
+      else
+        stowDir
+      fi
+  done
+
+}
+
+stowUpdateNoFoldingPkg () {
+
+  for package in ${stowPkgs[@]}
+    do
+      packageFirstSymbol=${package:0:1}
+
+      if [ "$packageFirstSymbol" = "_" ]; then
+        stowNoFolding
+      fi
+  done
+  
+}
+
+readArrays
+stowPkgExtract
