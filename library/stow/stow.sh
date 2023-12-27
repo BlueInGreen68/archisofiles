3828da605911bbcfd3d1dbc7941eab34a68cf131
blueingreen68 <blueingreen68@gmail.com>
2023-12-27 18:32:19 +0500

    update

--- library/stow/stow.sh
+++ library/stow/stow.sh
@@ -24,28 +24,28 @@ startStow () {
   if [ -d "$dotfiles" ]; then
       echo "✅ Папка .dotfiles есть"
   else
-      source $HOME/"$dirArchIsoFiles"/library/cloneDotfiles.sh
+      source "$dirArchIsoFiles"/library/cloneDotfiles.sh
   fi
 
   select event in "Stow extract" "Stow update" "Stow add" "Stow adopt" "Back"; do 
     case "$event" in
       "Stow extract")
-          source $HOME/"$dirArchIsoFiles"/library/stow/stowPkgExtract.sh
+          source "$dirArchIsoFiles"/library/stow/stowPkgExtract.sh
           break 2 
           ;;
 
       "Stow update")
-          source $HOME/"$dirArchIsoFiles"/library/stow/stowUpdateNoFoldingPkg.sh
+          source "$dirArchIsoFiles"/library/stow/stowUpdateNoFoldingPkg.sh
           break 2
           ;;
 
       "Stow add")
-          source $HOME/"$dirArchIsoFiles"/library/stow/stowAddPkg.sh
+          source "$dirArchIsoFiles"/library/stow/stowAddPkg.sh
           break 2 
           ;; 
 
       "Stow adopt")
-          source $HOME/"$dirArchIsoFiles"/library/stow/stowRewriteAbortedPkg.sh 
+          source "$dirArchIsoFiles"/library/stow/stowRewriteAbortedPkg.sh 
           break 2
           ;;
 
