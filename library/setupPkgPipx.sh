a22ed05e0784242b1ff5d6bf7ee2f108b7d98db1
blueingreen68 <blueingreen68@gmail.com>
2023-12-30 12:21:55 +0000

    update

--- library/setupPkgPipx.sh
+++ library/setupPkgPipx.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 
 setupPkgPipx () {
-  readarray -t pipxPkg < <(cat $HOME/"$dirArchIsoFiles"/pkglists/pipxpkglist.txt)
+  readarray -t pipxPkg < <(cat "$dirArchIsoFiles"/pkglists/pipxpkglist.txt)
 
   for pkg in ${pipxPkg[@]}; do
     pipx install "$pkg" 
